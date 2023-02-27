c3b20b494b9494aa49c302ac0802aaad019d2c15
Carlos Antonio Madrigal Trejo <imthewalrus32@gmail.com>
2023-02-27 09:18:17 -0600

    Basic JavaScript

--- JS_1.js
+++ JS_1.js
@@ -166,26 +166,26 @@ const conversation = 'Finn exclaims to Jake, "A la verga esta bien caro el kilo 
  let ourStr2 = "I come first. ";
  ourStr2 += "I come second.";
 
- //Constructing strings with variables
+ //Constructing strings with variables\\
 const ourName = "Carlos and Sofia";
 const ourStrName = "Hello, our name is " + ourName + ", how are you?";
 
-//Appending variables to Strings
+//Appending variables to Strings\\
 const anAdjective = "awesome!";
 let ourStrAdjective = "Coding is ";
 ourStrAdjective += anAdjective;
 
-//Find the lenght of a String
+//Find the lenght of a String\\
 console.log("Carlos Madrigal".length);
 const firstName = "Ada";
 console.log(firstName.length);
 
-//Use bracket notation to find the first character in a String
+//Use bracket notation to find the first character in a String\\
 //Bracket notation is a way to get a character at a specific index within a string
 const firstName2 = "Charles Darwin";
 const firstLetter = firstName2[0];
 
-//Understand String Immutability
+//Understand String Immutability\\
 //In JS String values are immutable, wich menas that they cannot be altered
 //once created, the following code will produce an error.
 
@@ -193,20 +193,20 @@ let myStrError = "Bob";
 myStr[0] = "J"; //wrong assigment
 myStr = "Job";
 
-//Use bracket notation to find the Nth Character in a String
+//Use bracket notation to find the Nth Character in a String\\
 //You can also use bracket notation to get the character at other position within a String.
 const firstName3 = "Ada";
 const secondLetterOfFirstName = firstName3[1];
 
-//Use bracket notation to Find the Last Character in a String
+//Use bracket notation to Find the Last Character in a String\\
 const firstName4 = "Anasofuwu";
 const lastLetter = firstName4[firstName4.length - 1];
 
-//Use bracket notation to Find the Nth-to-Last Character in a String
+//Use bracket notation to Find the Nth-to-Last Character in a String\\
 const firstName5 = "Margaret Trejo";
 const thirdToLastLetter = firstName5[firstName5.lenght - 3];
 
-//Word blanks
+//Word blanks\\
 const sentence = "It was really " + "hot" + ", and we " + "laughed" + " ourselves " + "silly" + ".";
 
 const myNoun = "dog";
@@ -218,32 +218,35 @@ const myAdverb = "quickly";
 const wordBlanks = "The " + myAdjective + " "+ myNoun + " " + myVerb + " " + myAdverb + "." // Change this line
 // Only change code above this line
 
-//Store Multiple Values in one Variable using JavaScript Arrays
+//Store Multiple Values in one Variable using JavaScript Arrays\\
 
 const sandwich = ["peanut butter", "jelly", "bread"];
 
 // Only change code below this line
 const myArray = ["league of legends" , 22];
 
-//Nest one Array within Another Array
+//Nest one Array within Another Array\\
+
 //You can also nest arrays within other arrays
 const teams = [["Bulls" , 23], ["White Sox", 45]]; //Also called multi-dimensional array
 
-//Access Array Data with Indexes
+//Access Array Data with Indexes\\
+
 //Array indexes are written in the same bracket notation that strings use, they specify
 //an entry in the arrey, they also use zero-based indexing
 const arrayIndexes = [50, 60, 70];
 console.log(array[0]);//prints 50
 const data = array[1];//prints 60
 
-//Modify Array Data With Indexes
+//Modify Array Data With Indexes\\
+
 //Unlike strings, the entries of arrays are mutable and can be changed freely,
 //even if the array was declared with const
 
 const ourArray = [50, 40, 30];
 ourArray[0] = 15;
 
-//Access Multi-Dimensional Arrays With Indexes
+//Access Multi-Dimensional Arrays With Indexes\\
 
 const arr = [[1,2,3], [4,5,6], [7,8,9], [[10,11,12],13,14]];
 
@@ -347,6 +350,282 @@ function fun2() {
   }
   console.log(output);
 }
+
+//Local Scope and Functions\\
+//Variables declared within a function have local scope, they are only visible within that function
+
+function myTest(){
+    const loc = "My love";
+    console.log(loc);
+}
+myTest();
+console.log(loc); //Throws an error due const loc is not reachable 
+
+//Global vs Local scope in functions\\
+//it is possible to have both local and global variables with the same name 
+//when you do this the local variables takes precedence over the global variables
+const someVar = "hat";
+function myFunction(){
+    const someVar = "head";
+    return someVar;
+}
+//The function will return head because the local version of the variable is present
+
+//Understanding undefined value returned from a function\\
+//When a function doesn't have a return statement the function processes the inner code but it returns an undefined value
+let sum = 0;
+
+function addSum(num){
+    sum = sum + num;
+}
+
+addSum(3);
+
+//The function will change the global variable sum but the returned value of the function is undefined 
+
+//Assignment with a return value\\
+//return statement can be used to assing a value to a variable 
+let processed = 0; 
+
+function processArg( num ){
+    return (num + 3) / 5;
+}
+
+processed = processArg(7);
+
+//Stand in line\\
+//In computer science queue is an abstract data struture where items are kept in order, new items can be added at the end of the queue
+//and old items are taken off from the front of the queue
+
+function nextInLine(arr, item) {
+    // Only change code below this line
+    arr.push(item);
+    return arr.shift();
+    // Only change code above this line
+  }
+  
+  // Setup
+  let testArr = [1, 2, 3, 4, 5];
+  
+  // Display code
+  console.log("Before: " + JSON.stringify(testArr));
+  console.log(nextInLine(testArr, 6));
+  console.log("After: " + JSON.stringify(testArr));
+
+//Use Conditional Logic with If Statements\\
+function test( logicCondition ){
+    if( logicCondition ){
+        return "it was true";
+    } else {
+        return "it was false";
+    }
+}
+
+test(true);
+test(false);
+
+//Comparision with equal operator\\
+/*There are many comparison operators in JavaScript. All of these operators return a boolean true or false value.The most basic operator is 
+the equality operator ==.The equality operator compares two values and returns true if they're equivalent or false if they are not.*/
+
+function equalityTest( myVal ){
+    if( myVal == 10 ){
+        return "Is equal to 10";
+    }
+    return "Is not equal to 10";
+}
+//In order for JavaScript to compare two different data types, it must convert one type to another. This is known as type coercion.
+//Once it does, however, it can compare terms as follows:
+1   ==  1  // true
+1   ==  2  // false
+1   == '1' // true
+"3" ==  3  // true
+
+//Comparison with the Strict Equality Operator\\
+/*Strinct equality is the counterpart of the equality operator. However, unlike equality operator, wich attempts to convert both values being 
+compared into a common type, the strict equality operator does not perform a type conversion,If the values being compared have different type,
+they are considered unequal*/
+
+3 ===  3  // true
+3 === '3' // false
+
+// Setup
+function testStrict(val) {
+    if (val === 7) { // Change this line
+      return "Equal";
+    }
+    return "Not Equal";
+  }
+  
+  testStrict(7);
+
+//Practice comparing different values\\
+function compareEquality(a, b){
+    if( a === b){
+        return "equal";
+    }
+    return "Not equal";
+}
+
+compareEquality(10, "10");
+
+//Comparison with the Inequality Operator\\
+//The inequality operator (!=) is the opposite of the equality operator.
+// It means not equal and returns false where equality would return true and vice versa. 
+//Like the equality operator, the inequality operator will convert data types of values while comparing.
+1 !=  2    // true
+1 != "1"   // false
+1 != '1'   // false
+1 != true  // false
+0 != false // false
+
+function testNotEqual(val){
+    if(val != 99){
+        return "Not equal";
+    }
+    return "Equal"
+}
+
+testNotEqual(10);
+
+//Comparison with the Strict Inequality Operator\\
+//The strict inequality operator (!==) is the logical opposite of the strict equality operator.
+//It means "Strictly Not Equal" and returns false where strict equality would return true and vice versa. 
+//The strict inequality operator will not convert data types.
+
+3 !==  3  // false
+3 !== '3' // true
+4 !==  3  // true
+
+function testStrictNotEqual(val){
+    if( val !== 17){
+        return "Not equal";
+    }
+    return "Equal";
+}
+
+testStrictNotEqual(10);
+
+//Comparison with the Greater Than Operator\\
+
+5   >  3  // true
+7   > '3' // true
+2   >  3  // false
+'1' >  9  // false
+
+function testGreaterThan(val){
+    if( val > 100){
+        return "Greater than 100";
+    }
+    if(val > 10){
+        return "Greater than 10";
+    }
+    return "10 or under";
+}
+
+testGreaterThan(10);
+
+//Comparison with the Greater Than Or Equal To Operator\\
+6   >=  6  // true
+7   >= '3' // true
+2   >=  3  // false
+'7' >=  9  // false
+
+function testGreaterOrEqual(val){
+    if( val >= 20){
+        return "20 or greater";
+    }
+    if( val >= 10){
+        return "10 or greater";
+    }
+    return "less than 10";
+}
+
+testGreaterOrEqual(10);
+
+//Comparison with the Less Than Operator\\
+2   < 5 // true
+'3' < 7 // true
+5   < 5 // false
+3   < 2 // false
+'8' < 4 // false
+
+function testLessThan(val) {
+    if (val < 25) {  // Change this line
+      return "Under 25";
+    }
+  
+    if (val < 55) {  // Change this line
+      return "Under 55";
+    }
+  
+    return "55 or Over";
+  }
+  
+  testLessThan(10);
+
+  //Comparison with the Less Than Or Equal To Operator\\
+4   <= 5 // true
+'7' <= 7 // true
+5   <= 5 // true
+3   <= 2 // false
+'8' <= 4 // false
+
+function testLessOrEqual(val) {
+    if (val <= 12) {  // Change this line
+      return "Smaller Than or Equal to 12";
+    }
+  
+    if (val <= 24) {  // Change this line
+      return "Smaller Than or Equal to 24";
+    }
+  
+    return "More Than 24";
+  }
+  
+  testLessOrEqual(10);
+
+  //Comparisons with the Logical And Operator\\
+  //The logical and operator (&&) returns true if and only if the operands to the left and right of it are true.\\
+  function betweenFiveAndTen(num){
+  if( num > 5){
+    if(num < 10){
+        return "Yes";
+    }
+  }
+  return "No";
+}
+
+function betweenFiveAndTen(num){
+    if( num > 5 && num < 10){
+        return "Yes";
+    }
+    return "No";
+    
+}
+
+function testLogicalAnd(val) {
+    // Only change code below this line
+  
+    if (val >= 25 && val <= 50) {
+      return "Yes";
+    }
+    // Only change code above this line
+    return "No";
+  }
+  
+  testLogicalAnd(10);
+
+  //Comparisons with the Logical Or Operator\\
+  
+
+
+
+ 
+
+
+
+
 
 
 
